193a4b6e73dacfcbfa468741eb2a9b727fd02181
corndev <corndev@mail.ru>
2025-04-29 06:02:33 +0300

    Update TutorialTrigger.cs

--- Assets/Scripts/TutorialTrigger.cs
+++ Assets/Scripts/TutorialTrigger.cs
@@ -1,21 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class TutorialTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public GameObject tutorial;
     private void OnTriggerEnter(Collider other)
     {
